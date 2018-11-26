51257cdd7a971f1c10463775e366c9dbf772c6dc
Inácio A. V. de Farias <inacio.avf@gmail.com>
2018-11-26 14:06:23 -0200

    Applies a better solution (by kewlar), found in the Community solutions tab

--- rail-fence-cipher/rail_fence_cipher.rb
+++ rail-fence-cipher/rail_fence_cipher.rb
@@ -1,12 +1,43 @@
-require_relative './rail_fence_cipher_encoder'
-require_relative './rail_fence_cipher_decoder'
-
 class RailFenceCipher # :nodoc:
-  def self.encode(text, rails_count)
-    RailFenceCipherEncoder.new(rails_count).encode(text)
+  VERSION = 1
+
+  def self.encode(message, rail_count)
+    lookup = generate_lookup(message.length, rail_count)
+
+    transcode(message, lookup)
   end
 
-  def self.decode(text, rails_count)
-    RailFenceCipherDecoder.new(rails_count).decode(text)
+  def self.decode(message, rail_count)
+    lookup = generate_lookup(message.length, rail_count)
+
+    transcode(message, reverse_lookup(lookup))
+  end
+
+  def self.transcode(message, lookup)
+    lookup.map { |index| message[index] }.join
+  end
+
+  def self.generate_lookup(length, count)
+    rails = Array.new(count) { [] }
+    (0...length).each do |index|
+      rails[rail_index(index, count)] << index
+    end
+
+    rails.flatten
+  end
+
+  def self.reverse_lookup(lookup)
+    lookup
+      .each_with_index
+      .each_with_object([]) do |(to, from), reverse_lookup|
+        reverse_lookup[to] = from
+      end
+  end
+
+  def self.rail_index(char_index, rail_count)
+    idx = char_index % [1, rail_count * 2 - 2].max
+    idx = rail_count - (idx - rail_count) - 2 if idx >= rail_count
+
+    idx
   end
 end
