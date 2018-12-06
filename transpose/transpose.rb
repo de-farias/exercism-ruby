8d665d3f427adc66ddaa5a58a3ad07b0aeda65c6
Inácio A. V. de Farias <inacio.avf@gmail.com>
2018-12-06 14:30:24 -0200

    Implements the Transpose exercise

---
+++ transpose/transpose.rb
@@ -0,0 +1,18 @@
+class Transpose # :nodoc:
+  def self.transpose(text)
+    return text if text.empty?
+
+    text_on_rails = normalized_text_rows(text)
+    text_on_rails.transpose.map(&:join).map(&:rstrip).join("\n")
+  end
+
+  def self.normalized_text_rows(text)
+    splitted_text = text.split("\n").map(&:chars)
+    largest = splitted_text.max_by(&:length).length
+
+    splitted_text.map do |row|
+      row.fill(' ', row.length, largest - row.length) if row.length < largest
+      row
+    end
+  end
+end
